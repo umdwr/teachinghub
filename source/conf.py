55729f1301a0141654f8556531d0d4273e3970ec
adavis46 <adavis46@outlook.com>
2021-07-20 08:25:34 -0500

    add favicon

--- source/conf.py
+++ source/conf.py
@@ -54,7 +54,7 @@ html_theme_options = {
 
 html_logo = "assets/logo.png"
 
-html_favicon = "https://olemiss.edu/apple-touch-icon.png"
+html_favicon = "assets/favicon.ico"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
